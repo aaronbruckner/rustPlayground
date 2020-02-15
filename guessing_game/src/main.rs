e7d517c09c24c98dd38bb62aa224a04786c93a94
Aaron Bruckner <aaronbruckner@gmail.com>
2020-02-15 12:37:16 -0800

    Finished my first rust guessing game.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_num = rand::thread_rng().gen_range(1, 101);
+    println!("Guess the number!");
+
+    loop {
+        println!("Please enter your number...");
+
+        let mut guess: String = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
 }
